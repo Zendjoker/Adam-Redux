7446d5556c4cfa699a39c7d47eaef671a85286a0
Zendjoker <adoumazzzouz.aa@gmail.com>
2021-12-16 22:45:50 +0100

    adam

---
+++ src/Components/AddTodo/AddTodo.js
@@ -0,0 +1,31 @@
+import React,{useState} from 'react'
+import './AddTodo.css'
+
+    // add newTodo
+const AddTodo = ({createTodo}) => {
+    const [task,setTask]=useState('');
+    const handleSumbit =(e)=>{
+        e.preventDefault();
+        if(task !== "") {
+        createTodo(task);
+        setTask("");
+    }else{
+        alert('Can not add an empty text')
+    }
+    }
+    return (
+        <>
+        <p className='description'>Add new Task</p> <br/>
+        <form className='TodoInput' onSubmit={handleSumbit}>
+            <input
+                type='text'
+                placeholder='write your Todo here'
+                value={task}
+                onChange={(e)=>setTask(e.target.value)}
+            />
+            <button>Add Todo</button>
+        </form>
+        </>
+    )
+}
+export default AddTodo
